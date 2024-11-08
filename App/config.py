ec9c4f9303982cb976e6017103dcf53dadbd1873
hysin <invidiia@163.com>
2024-11-08 18:15:42 +0800

    2nd commit

--- App/config.py
+++ App/config.py
@@ -5,11 +5,7 @@ SECRET_KEY = 'happydays'
 PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=10)
 
 # DB
-<<<<<<< HEAD
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mm546896@localhost:3306/demo1'
-=======
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost:3306/db'
->>>>>>> 03c102967b419fe8034f5b4727915c7fa85f78f1
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # redis
